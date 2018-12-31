abb6c2f8f96461b15d04707f440fa7ce8ea32183
Teba Gomez <gomez.teba@gmail.com>
2018-12-31 13:42:36 +0000

    Documentation and refactor. Close #9

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 // Variables
-
 const scoreSlctr = document.getElementById("score-value");
 const lifeSlctr = document.getElementById("life-value");
 const modalSlctr = document.getElementById("modal");
@@ -20,12 +19,18 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function showModal(){
-    modalSlctr.style.display = "block"; 
+/**
+ * Show the Game Over modal.
+ */
+function showModal() {
+    modalSlctr.style.display = "block";
 };
 
-function hideModal (){
-    modalSlctr.style.display = "none"; 
+/**
+ * Hide the Game Over modal.
+ */
+function hideModal() {
+    modalSlctr.style.display = "none";
 };
 
 /**
@@ -46,78 +51,87 @@ class Enemy {
      * Create an enemy.
      * @param {number} y - The y value.
      */
-    constructor(y){
+    constructor(y) {
         this.sprite = 'images/enemy-bug.png';
-        this.x = -100;  
-        this.y = y; 
+        this.x = -100;
+        this.y = y;
         this.speed = getRandomInt(50, 400);
     }
     /**
      * Update the enemy's position.
      * @param {number} dt - a time delta between ticks
      */
-    update(dt){
+    update(dt) {
         /* 
         If the enemy is outside the board, it returns to the same spot it started. 
         Else, the acceleration of the enemy' movement.     
-        */ 
+        */
         if (this.x >= 600) {
-            this.x = -100; 
+            this.x = -100;
         } else {
-            this.x += (dt*this.speed);
+            this.x += (dt * this.speed);
         }
     }
     /**
      * Draw the enemy on the screen.
      */
-    render () {
+    render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 }
 
 /** Class representing the player. */
 class Player {
-     /**
+    /**
      * Create the player.
      */
-    constructor(){
+    constructor() {
         this.sprite = 'images/char-cat-girl.png';
-        this.x = 200;  
+        this.x = 200;
         this.y = 400;
         this.yStep = 85;
-        this.xStep = 100; 
+        this.xStep = 100;
         this.score = 0;
         this.scoreStep = 100;
-        this.life = 3; 
-    }
-    
-    updateScore(){
-        scoreSlctr.innerText = `Score = ${this.score}`;
-    }
-    
-    updateLives(){
-        lifeSlctr.innerText = `${this.life}`
+        this.life = 3;
     }
     /**
-     * Update the player's position.
+     * Update the player' score.
      */
-    update(){
-        const mapLimits = [ this.x >= 0, this.x <= 400, this.y >= -25, this.y <= 400 ];
+    updateScore() {
+        scoreSlctr.innerText = `Score = ${this.score}`;
+    }
+
+    /**
+     * Update the player's lives.
+     */
+    updateLives() {
+        lifeSlctr.innerText = `${this.life}`
+    }
+
+    /**
+     * Update the player's position.
+     * - Collision detecton
+     * - Update life counter
+     * - Update score counter
+     */
+    update() {
+        const mapLimits = [this.x >= 0, this.x <= 400, this.y >= -25, this.y <= 400];
         // The player hits at least one enemy
         if (allEnemies.some(enemy => {
-            const sameY = enemy.y === this.y;
-            const playerArea = {
-                min: this.x - (this.xStep/2),
-                max: this.x + (this.xStep/2)
-            }; 
-            const sameArea = enemy.x >= playerArea.min && enemy.x <= playerArea.max;
-            return sameY && sameArea;
-        })){
+                const sameY = enemy.y === this.y;
+                const playerArea = {
+                    min: this.x - (this.xStep / 2),
+                    max: this.x + (this.xStep / 2)
+                };
+                const sameArea = enemy.x >= playerArea.min && enemy.x <= playerArea.max;
+                return sameY && sameArea;
+            })) {
             // Life counter
             this.life--;
-            this.score = 0; 
-            
-            if (this.life === 0){
+            this.score = 0;
+
+            if (this.life === 0) {
                 showModal();
                 this.life = 3;
             }
@@ -125,64 +139,52 @@ class Player {
             this.updateScore();
             this.reset();
         }
-        
+
         // Player reaches water!
         if (this.y <= 0) {
-            
+
             // Score Text
-            this.score += this.scoreStep; 
+            this.score += this.scoreStep;
             this.updateScore();
             this.reset();
         }
-        if (!mapLimits.every(limit => limit)){
+        if (!mapLimits.every(limit => limit)) {
             this.reset();
         }
     }
     /**
      * Draw the player on the screen.
      */
-    render(){
+    render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
     /**
      * Reset the player to the initial position.
      */
     reset() {
-        this.x = 200;  
+        this.x = 200;
         this.y = 400;
     }
     /**
      * Applies coordinates of player' movements through the keys.
      * @param {string} cmd - the pressed key.  
      */
-    handleInput(cmd){
-        if (cmd === 'up'){
-            this.y -= this.yStep; 
-        } else if (cmd === 'down'){
-            this.y += this.yStep; 
-        } else if (cmd === 'right'){
-            this.x += this.xStep; 
-        } else if (cmd === 'left'){
-            this.x -= this.xStep; 
+    handleInput(cmd) {
+        if (cmd === 'up') {
+            this.y -= this.yStep;
+        } else if (cmd === 'down') {
+            this.y += this.yStep;
+        } else if (cmd === 'right') {
+            this.x += this.xStep;
+        } else if (cmd === 'left') {
+            this.x -= this.xStep;
         }
     }
 }
 
-// Now instantiate your objects.
-
-// Place all enemy objects in an array called allEnemies
-const floors = [60,145,230] // `y` values for each floor
-// We generate 3 new Enemyes for now...
-const allEnemies = duplicateElements(floors).map(y => new Enemy(y));
-
-// Place the player object in a variable called player
-const player = new Player();
-
-
 // Events
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// This listens for key presses and sends the keys to player.handleInput() method. 
 document.addEventListener('keyup', e => {
     const allowedKeys = {
         37: 'left',
@@ -194,7 +196,13 @@ document.addEventListener('keyup', e => {
     player.handleInput(allowedKeys[e.keyCode]);
 });
 
-// buttonSlctr
-buttonSlctr.addEventListener("click", () => {
-    hideModal();
-});
+// Listens to the button click and hide the Game Over modal. 
+buttonSlctr.addEventListener("click", hideModal);
+
+// This array contains the enemies `y` position in the canvas' rows.
+const rows = [60, 145, 230];
+// We generate 6 new Enemyes for now (2 per row).
+const allEnemies = duplicateElements(rows).map(y => new Enemy(y));
+
+// The player generation. 
+const player = new Player();
